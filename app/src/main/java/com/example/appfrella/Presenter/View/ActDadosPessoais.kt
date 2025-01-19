9ffe4128b069adb75e076fd1dff30213f268a2c0
RafaelKontein23 <rafael.kontein23@gmail.com>
2025-01-19 20:56:09 -0300

    ajuste de itens

---
+++ app/src/main/java/com/example/appfrella/Presenter/View/ActDadosPessoais.kt
@@ -0,0 +1,27 @@
+package com.example.appfrella.Presenter.View
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.appfrella.R
+import com.example.appfrella.databinding.ActivityActDadosPessoaisBinding
+
+class ActDadosPessoais : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityActDadosPessoaisBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
